7aa2e8e31211553cdb613b102eec88171b3c3581
unknown <ataidi>
2026-03-30 15:25:58 +0300

    online booking

---
+++ models.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String
+from connections import Base
+
+class Todo(Base):
+    __tablename__="online_tbl"
+    id=Column(Integer,primary_key=True,autoincrement=True)
+    title=Column(String(100))
+    description=Column(String(100))
